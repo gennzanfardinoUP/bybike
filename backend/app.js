f981a19a9a7c96985568d0f080a0813e9f46d619
gennzanfardinoUP <up914294@myport.ac.uk>
2019-03-29 21:45:25 +0000

    working with up864461
    we managed to replace php and mysql with nodejs and mongoDB

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+
+
+const express = require('express');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const mongoose = require('mongoose');
+
+const Thing = require('./models/thing');
+
+const stuffRoutes = require('./routes/stuff');
+
+const userRoutes  = require('./routes/user');
+
+mongoose.connect('mongodb+srv://root:root@team3d-pscfn.mongodb.net/test?retryWrites=true')
+  .then(() => {
+    console.log('Successfully connected to MongoDB Atlas!');
+  })
+  .catch((error) => {
+    console.log('Unable to connect to MongoDB Atlas!');
+    console.error(error);
+  });
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth',  userRoutes);
+
+
+
+
+module.exports = app;
