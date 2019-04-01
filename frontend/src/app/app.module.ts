a3d17a6aa75886e57e054f213166cf6e07d86584
gennzanfardinoUP <up914294@myport.ac.uk>
2019-04-01 14:16:40 +0100

    Integrating

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,27 +1,55 @@
-import { LocationsComponent } from './locations.component';
-import { LocationsService } from './locations.service';//manual import cause of lacking auto-import
-
-
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LocationComponent } from './location/location.component';
+import { PartOneComponent } from './part-one/part-one.component';
+import { PartThreeComponent } from './part-three/part-three.component';
+import { PartFourComponent } from './part-four/part-four.component';
+import { DefaultComponent } from './default/default.component';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { StuffListComponent } from './part-one/stuff-list/stuff-list.component';
+import { NewThingComponent } from './part-one/new-thing/new-thing.component';
+import { SingleThingComponent } from './part-one/single-thing/single-thing.component';
+import { ModifyThingComponent } from './part-one/modify-thing/modify-thing.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatProgressSpinnerModule } from '@angular/material';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { NewThingWithUploadComponent } from './part-four/new-thing-with-upload/new-thing-with-upload.component';
+import { ModifyThingWithUploadComponent } from './part-four/modify-thing-with-upload/modify-thing-with-upload.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/auth-interceptor';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LocationsComponent,
-    LocationComponent
+    PartOneComponent,
+    PartThreeComponent,
+    PartFourComponent,
+    DefaultComponent,
+    HeaderComponent,
+    StuffListComponent,
+    NewThingComponent,
+    SingleThingComponent,
+    ModifyThingComponent,
+    LoginComponent,
+    SignupComponent,
+    NewThingWithUploadComponent,
+    ModifyThingWithUploadComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatProgressSpinnerModule,
+    HttpClientModule
   ],
-  providers: [
-    LocationsService
-  ],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
